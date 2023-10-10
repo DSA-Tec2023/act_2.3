659d569269ce36ce4dd5c1f467302a25f726351b
Andrés Martínez <autummata@gmail.com>
2023-10-09 22:07:06 -0700

    added monthly func

--- main.cpp
+++ main.cpp
@@ -1,17 +1,48 @@
 #include <iostream>
 #include <fstream>
 #include "LinkedList.hpp"
+#include <string>
+#include <map>
 
-/*void read_and_get_bitacora_info(LinkedList& listaM, LinkedList& listaR, std::string file_name){
+std::string get_month(int number) { 
+    std::map<int, std::string> months = {
+        {1, "January"},
+        {2, "February"},
+        {3, "March"},
+        {4, "April"},
+        {5, "May"},
+        {6, "June"},
+        {7, "July"},
+        {8, "August"},
+        {9, "September"},
+        {10, "October"},
+        {11, "November"},
+        {12, "December"}
+    };
+
+    if (months.find(number) != months.end()) {
+        return months[number]; 
+
+    } else {
+        return "";  
+    }
+
+}
+
+
+void read_and_get_bitacora_info(LinkedList& listaM, LinkedList& listaR, std::string file_name){
     std::ifstream bitacora_file(file_name);
     std::string line;
     while (std::getline(bitacora_file, line)) {
-        if(line[17] == "M")
-            listaM.create_beginning(line)
-        else
-            listaR.create_beginning(line)
+        if(line[17] == 'M') {
+            listaM.create_beginning(line);
+        }
+        else {
+            listaR.create_beginning(line);
+        }
     }
-}*/
+    return;
+}
 
 int main() { 
     std::cout << "Hello, world!" << std::endl;
@@ -20,6 +51,20 @@ int main() {
 
     list.create_position(1, "andres"); 
     list.display_list();
+
+    LinkedList listaM;
+    LinkedList listaR;
+    read_and_get_bitacora_info(listaM, listaR, "canal.txt");
+    
+    std::cout << "Lista M" << std::endl;
+    listaM.display_list();
+
+    std::cout << "Lista R" << std::endl;
+    listaR.display_list();
+
+    std::string month = get_month(8);
+    std::cout << "Month: " << month << std::endl;
+
 
     return 0; 
 }
