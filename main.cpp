206d99d075ecced7a886023fb85f6c84b030ae73
FernandaGranadosMonge <138026408+FernandaGranadosMonge@users.noreply.github.com>
2023-10-10 00:28:30 +0000

    PropuestaRead

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <fstream>
 #include "LinkedList.hpp"
+
+/*void read_and_get_bitacora_info(LinkedList& listaM, LinkedList& listaR, std::string file_name){
+    std::ifstream bitacora_file(file_name);
+    std::string line;
+    while (std::getline(bitacora_file, line)) {
+        if(line[17] == "M")
+            listaM.create_beginning(line)
+        else
+            listaR.create_beginning(line)
+    }
+}*/
 
 int main() { 
     std::cout << "Hello, world!" << std::endl;
@@ -7,7 +19,7 @@ int main() {
     list.create_beginning(1);
 
     list.create_position(1, 3); 
-    list.display_list(); 
+    list.display_list();
 
     return 0; 
 }
